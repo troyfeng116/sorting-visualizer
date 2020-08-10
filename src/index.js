584cff9669a741c443c4f5158c12209a62377657
Troy Feng <troy.feng@yale.edu>
2020-08-10 18:05:30 -0400

    Playing with displaying bars and incorporating React into output updateing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Car extends React.Component {
+class Bar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state={highlight:"none"};
+	}
 	render() {
 		return (
-			<h1>Hello!</h1>
+			<div>Bruh</div>
 		);
 	}
 }
 
-ReactDOM.render(<Car />, document.getElementById("root"));
+var N;
+const barContainer = document.getElementById("barContainer");
+
+const barSlider = document.getElementById("barSlider")
+barSlider.onchange = function() {
+	N = barSlider.value;
+	document.getElementById("numBarsDisplay").innerHTML = N;
+	for (let i = 0; i < N; i++) {
+		var bar = document.createElement("div");
+		bar.innerHTML = i;
+		bar.style.height = i*10 + "px";
+		barContainer.appendChild(bar);
+	}
+}
+
+//ReactDOM.render(<Bar val="1"  />, document.getElementById("barContainer"));
