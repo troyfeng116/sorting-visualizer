b9c74bc8320dc37e77ac448b4a070be9f017e089
Troy Feng <troy.feng@yale.edu>
2020-08-13 19:03:16 -0400

    Implemented bar display and styling using React components and JS styling vars

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class Bar extends React.Component {
 
 	render() {
 		return (
-			<div>DIV {this.props.val}</div>
+			<div style={this.props.style}>DIV {this.props.val}</div>
 		);
 	}
 }
@@ -19,19 +19,25 @@ class BarContainer extends React.Component {
 		super(props);
 		this.state= {
 			numBars: 10,
-			barArray: [1,2,3,4,5,6,7,8,9,10],
+			barArray: [1,2,3,4,5,6,7,9,10,8],
 		};
 	}
-	renderBar(i) {
+	renderBar(val,style) {
 		return (
-			<Bar val={i} />
+			<Bar val={val} style={style} />
 		);
 	}
 
 	render() {
 		const bars = this.state.barArray;
-		const moves = bars.map((index) => {
-      		return this.renderBar(index);
+		const n = bars.length;
+		const moves = bars.map((val,index) => {
+			const barStyle = {
+				width:100/n+"%",
+				height:val/n*100+"%",
+				left:index*100/n+"%",
+			};
+      		return this.renderBar(val,barStyle);
     	});
 
 		return (
