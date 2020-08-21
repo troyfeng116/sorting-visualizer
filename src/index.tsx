805503ae3a567330c26abc497dfb05fd60b3cb6a
Troy Feng <troy.feng@yale.edu>
2020-08-20 23:26:55 -0400

    Began implementing functional components

--- src/index.tsx
+++ src/index.tsx
@@ -18,37 +18,30 @@ type BarProps = {
 	style: React.CSSProperties,
 }
 
+const Bar = (props: BarProps) => <div style={props.style}>{props.val}</div>;
+
 type BarContainerProps = {
 	numBars: number,
 	barArray: number[][]
 }
 
-const Bar = (props: BarProps) => <div style={props.style}>{props.val}</div>;
-
-class BarContainer extends React.Component<BarContainerProps,{}> {
-	renderBar(val: number|string, style: React.CSSProperties) {
-		return (
-			<Bar val={val} style={style} />
-		);
-	}
-	render() {
-		const bars = this.props.barArray;
-		const n = this.props.numBars;
-		const moves = bars.map((val:number[],index:number) => {
-			const barStyle = {
-				width:55/n+"%",
-				height:val[0]/n*100+"%",
-				left:index*100/n+"%",
-				backgroundColor:getColor(val[1])
-			};
-      		return n <= 40? this.renderBar(val[0],barStyle) : this.renderBar("",barStyle);
-    	});
-		return (
-			<div id="barContainer">
-				{moves}
-			</div>
-		);
-	}
+const BarContainer = (props: BarContainerProps) => {
+	const bars = props.barArray;
+	const n = props.numBars;
+	const moves = bars.map((val:number[],index:number) => {
+		const barStyle = {
+			width:55/n+"%",
+			height:val[0]/n*100+"%",
+			left:index*100/n+"%",
+			backgroundColor:getColor(val[1])
+		};
+      	return n <= 40? <Bar val={n} style={barStyle} /> : <Bar val={""} style={barStyle} />;
+    });
+	return (
+		<div id="barContainer">
+			{moves}
+		</div>
+	);
 }
 
 class BigContainer extends React.Component {
