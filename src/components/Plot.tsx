8f7122db30a1ad396b59bd7d11521c79fee45cbc
Troy Feng <troy.feng@yale.edu>
2020-08-23 14:52:37 -0400

    Added axes and adjusted position of datapoints

--- src/components/Plot.tsx
+++ src/components/Plot.tsx
@@ -6,15 +6,19 @@ const Plot = (props: {data: any[][]}) => {
 	function drawChart() {
 		const dataset = props.data;
 		d3.select("#chart").remove();
-		const numPoints = 135;
+		const numPoints = 130;
 		const maxRuntime = 5000;
-		var svg = d3.select("body").append("svg").attr("width","50%").attr("height","50%").attr("id","chart");
+		const height = 800;
+		const width = 800;
+		var svg = d3.select("body").append("svg").attr("width",width).attr("height",height).attr("id","chart");
 		svg.selectAll("circle")
 			.data(dataset)
 			.enter()
 			.append("svg:circle")
-			.attr("cx", (d) => (100/numPoints)*d[0]+"%")
-			.attr("cy", (d) => (50)+"%")
+			.attr("cx", (d) => (d[0]/numPoints*(width-100)))
+			.attr("transform", "translate(50,"+(height/2+10)+")")
+			.attr("cy", (d) => (height/2 - d[1]/maxRuntime*(height/2)))
+			.attr("transform", "translate(50,10)")
 			.attr("r", 4)
 			.attr("fill", (d) => getColor(d[2]));
 		svg.selectAll("text")
@@ -22,22 +26,38 @@ const Plot = (props: {data: any[][]}) => {
 			.enter()
 			.append("text")
 				.text((d) => d[2] + ": (" + d[0] + "," + d[1] + ")")
-				.attr("x", (d) => (100/numPoints)*d[0]+"%")
-				.attr("y", (d) => (50)+"%")
+				.attr("x", (d) => (d[0]/numPoints*(width-100))+10)
+				.attr("transform", "translate(50,"+(height/2+10)+")")
+				.attr("y", (d) => (height/2 - d[1]/maxRuntime*(height/2)))
+				.attr("transform", "translate(50,10)")
 				.attr("font-size", "11px")
-				.attr("fill", "red");
+				.attr("fill", (d) => getColor(d[2]));
+		var xScale = d3.scaleLinear()
+			.domain([0,numPoints])
+			.range([0, width-100]);
+		var xAxis = d3.axisBottom(xScale);
+		var yScale = d3.scaleLinear()
+			.domain([0,maxRuntime])
+			.range([height/2,0]);
+		var yAxis = d3.axisLeft(yScale);
+		svg.append("g")
+			.attr("transform", "translate(50,10)")
+			.call(yAxis);
+		svg.append("g")
+			.attr("transform", "translate(50,"+(height/2+10)+")")
+			.call(xAxis);
 	}
-	return <div>{props.data}</div>;
+	return <div></div>;
 };
 
 function getColor(algo:string) {
 	switch (algo) {
 		case 'bSort': return "red";
 		case 'iSort': return "orange";
-		case 'hSort': return "yellow";
+		case 'hSort': return "pink";
 		case 'qSort': return "green";
 		case 'mSort': return "blue";
-		case 'shuffle': return "purple";
+		case 'shuffle': return "black";
 	}
 	return "black";
 }
