bc9a764a9812c683a7f8ada38adab9957dde2523
Troy Feng <troy.feng@yale.edu>
2020-08-24 10:15:39 -0400

    Updates

---
+++ precache-manifest.45fb3872645bc9716ccac048380d0442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6e16298e8ca08f0b2df7adb6c37f37",
+    "url": "/sorting-visualizer/index.html"
+  },
+  {
+    "revision": "e9fd371b2a2f2d4acd13",
+    "url": "/sorting-visualizer/static/css/main.4bc0321f.chunk.css"
+  },
+  {
+    "revision": "dfc97b8c9a5b40587626",
+    "url": "/sorting-visualizer/static/js/2.878c184b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sorting-visualizer/static/js/2.878c184b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fd371b2a2f2d4acd13",
+    "url": "/sorting-visualizer/static/js/main.c5905bf1.chunk.js"
+  },
+  {
+    "revision": "0b849e312d4bb7a17285",
+    "url": "/sorting-visualizer/static/js/runtime-main.648a2344.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.529bbbc4ad10fdac5199ef18292d8bf6.js"
+  "/sorting-visualizer/precache-manifest.45fb3872645bc9716ccac048380d0442.js"
 );
 
 self.addEventListener('message', (event) => {
