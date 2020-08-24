ae2430954de8cb68728de8268aa25b9b44593db0
Troy Feng <troy.feng@yale.edu>
2020-08-24 11:33:49 -0400

    Added min, max, step options in individual sampling function

--- src/algorithms/sample.ts
+++ src/algorithms/sample.ts
@@ -12,21 +12,21 @@ export function generateSample(algo:string) {
 	var results:any[][] = [];
 	var index = algoStrings.indexOf(algo);
 	if (index >= 0) {
-		let seq = oneAlgoSample(algo);
+		let seq = oneAlgoSample(algo, 5, 128, 5);
 		seq.forEach((point) => results.push(point));
 	}
 	else {
 		for (let i = 0; i < 6; i++) {
-			let seq = oneAlgoSample(algoStrings[i]);
+			let seq = oneAlgoSample(algoStrings[i], 10, 120, 10);
 			seq.forEach((point) => results.push(point));
 		}
 	}
 	return results;
 }
 
-function oneAlgoSample(algo:string) {
+function oneAlgoSample(algo:string, min:number, max:number, step:number) {
 	var results:any[][] = [];
-	for (let n = 10; n <= 120; n+=10) {
+	for (let n = min; n <= max; n+=step) {
 		var arr = makeArray(n);
 		fisher_yeats(arr);
 		var newSeq = algorithms[algoStrings.indexOf(algo)](arr);
